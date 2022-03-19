440c582fe50cb6c85d3787e286b37030923dd969
naufalchairurrijal <naufalalifaza@student.telkomuniversity.ac.id>
2022-03-19 10:08:19 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ KPL_MOD4_1302200022_RFL/Penjumlahan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPL_MOD4_1302200022_RFL
+{
+    internal class Penjumlahan
+    {
+        public static T JumlahTigaAngka<T>(double v, T a, T b, T c)
+        {
+            dynamic TempA = a;
+            dynamic TempB = b;
+            dynamic TempC = c;
+
+            return TempA + TempB + TempC;
+        }
+    }
+}
--- KPL_MOD4_1302200022_RFL/Program.cs
+++ KPL_MOD4_1302200022_RFL/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using KPL_MOD4_1302200022_RFL;
+
+Penjumlahan Penjumlahan = new Penjumlahan();
+
+//nim = 1302200022 berakhiran 2 sehingga menggunakan float
+Console.WriteLine(Penjumlahan.JumlahTigaAngka<float>(1.3f, 2.2f, 2,2f));
